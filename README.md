1c37dba82a03f294dc8b4a33cc761c89680016a5
zemnmez <zemnmez@googlemail.com>
2018-01-05 22:43:06 +0000

    local code for dark web support

---
+++ README.md
@@ -0,0 +1,3 @@
+#Dark Web Browser
+Used for browsing the 'dark web' in chrome, with all the usual security benefits of
+making it hard for others to see what you're doing.
---
+++ darkweb-shim.js
@@ -0,0 +1,1 @@
+document.body.appendChild(document.createElement("div")).setAttribute("style","background-color:black;opacity:.5;width:100%;height:100%;position:fixed;top:0;left:0;pointer-events:none");
