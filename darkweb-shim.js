200ba3a495366e317e8de60866286948d225e37a
zemnmez <zemnmez@googlemail.com>
2018-01-05 22:46:17 +0000

    fix issue where dark web code would only engague shortly after load for some sites, and some elements would be loaded outside of the dakr web

--- darkweb-shim.js
+++ darkweb-shim.js
@@ -1,1 +1,1 @@
-document.body.appendChild(document.createElement("div")).setAttribute("style","background-color:black;opacity:.5;width:100%;height:100%;position:fixed;top:0;left:0;pointer-events:none");
+document.documentElement.appendChild(document.createElement("div")).setAttribute("style","background-color:black;opacity:.5;width:100%;height:100%;position:fixed;top:0;left:0;pointer-events:none;z-index:9999");
